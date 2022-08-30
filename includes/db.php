98a28ddd833b9b081a6f7dd90f7b9be607af8156
Aashish Bansal <aashish22bansal@gmail.com>
2022-08-30 17:42:36 +0530

    Changes to Database File
    Changes to Database File in db.php

--- includes/db.php
+++ includes/db.php
@@ -1,3 +1,21 @@
 <?php
-    $db = mysqli_connect("sql304.epizy.com","epiz_32266738","OEoGCO0mWksXpN","epiz_32266738_myportfolio") or die("Database Not Found!");
+    $servername = "localhost";
+    $username = "root";
+    $password = "123Aashish456";
+    $databaseName = "myportfolio";
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $db = mysqli_connect($servername,$username,$password,$databaseName,"8080") or die("Database Not Found!");
+    // Create connection
+    // $conn = new mysqli('localhost','root',$password,$databaseName);
+    // Check connection
+    if ($db->connect_error) {
+        die("Connection failed: " . $db->connect_error);
+    }
+    // $mysqli = new mysqli($servername,$username,$password,$databaseName);
+
+    // // Check connection
+    // if ($mysqli -> connect_errno) {
+    //     echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+    //     exit();
+    // }
 ?>
